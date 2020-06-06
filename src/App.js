00547b6c5e51d3edf0c12d9708ba372e70e3e60e
Sudarpo Chong <sudarpo.chong@gmail.com>
2020-06-06 22:10:03 +0800

    2 Added font-awesome, Handle mark-complete event, and delete-event.

--- src/App.js
+++ src/App.js
@@ -7,15 +7,40 @@ export default class App extends Component {
     Tasks: InitialTasksList,
   };
 
+  handleTaskComplete = (task) => {
+    const allTasks = [...this.state.Tasks];
+    const index = allTasks.findIndex((t) => t.id === task.id);
+    const task1 = { ...allTasks[index] };
+    task1.completed = true;
+    allTasks[index] = task1;
+    this.setState({ Tasks: allTasks });
+  };
+
+  handleTaskDelete = (task) => {
+    let filtered = [...this.state.Tasks].filter((t) => t.id !== task.id);
+    this.setState({ Tasks: filtered });
+  };
+
+  handleTaskEdit = (task) => {
+    console.log("Edit", task);
+  };
+
   render() {
+    const { Tasks } = this.state;
+
     return (
       <div className="container">
         <div className="row">
           <div className="col">
-            <h1>INPUT FORM HERE</h1>
+            <ToDoList
+              Tasks={Tasks}
+              onTaskComplete={this.handleTaskComplete}
+              onTaskDelete={this.handleTaskDelete}
+              onTaskEdit={this.handleTaskEdit}
+            />
           </div>
           <div className="col">
-            <ToDoList Tasks={this.state.Tasks} />
+            <h1>INPUT FORM HERE</h1>
           </div>
         </div>
       </div>
--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,20 +1,77 @@
 import React from "react";
 
 export default function ToDoList(props) {
+  const { Tasks, onTaskComplete, onTaskDelete, onTaskEdit } = props;
+  const countPending = Tasks.filter((t) => t.completed === false).length;
+  const countCompleted = Tasks.length - countPending;
+
+  const renderAction = (task) => {
+    return (
+      <span className="float-right">
+        {task.completed ? (
+          <span className="badge badge-success m-1">done</span>
+        ) : (
+          <>
+            <span className="badge badge-warning m-1"></span>
+            <button
+              type="button"
+              title="Edit"
+              className="btn btn-sm btn-secondary mr-1"
+              onClick={() => onTaskEdit(task)}
+            >
+              <i className="fa fa-pencil" aria-hidden="true"></i>
+            </button>
+            <button
+              type="button"
+              title="Mark Complete"
+              className="btn btn-sm btn-primary"
+              onClick={() => onTaskComplete(task)}
+            >
+              <i className="fa fa-check" aria-hidden="true"></i>
+            </button>
+          </>
+        )}
+
+        <button
+          type="button"
+          title="Delete"
+          className="btn btn-sm btn-danger m-1"
+          onClick={() => onTaskDelete(task)}
+        >
+          <i className="fa fa-trash-o"></i>
+        </button>
+      </span>
+    );
+  };
+
   return (
-    <div className="mt-2 list-group">
-      {props.Tasks.map((task) => {
-        return (
-          <div className="list-group-item" key={task.id}>
-            {task.title}
-            {task.completed ? (
-              <span className="badge badge-success float-right">[done]</span>
-            ) : (
-              <span className="badge badge-warning float-right">[pending]</span>
-            )}
+    <>
+      <div className="card">
+        <div className="card-header">
+          {Tasks.length} items ~ {countPending} pending ~ {countCompleted}{" "}
+          completed
+        </div>
+        <div className="card-body">
+          <div className="list-group">
+            {Tasks.map((task) => {
+              return (
+                <div className="list-group-item" key={task.id}>
+                  <i className="fa fa-dot-circle-o mr-2" aria-hidden="true"></i>
+                  {task.completed ? (
+                    <>
+                      <del>{task.title}</del>
+                    </>
+                  ) : (
+                    <>{task.title}</>
+                  )}
+
+                  {renderAction(task)}
+                </div>
+              );
+            })}
           </div>
-        );
-      })}
-    </div>
+        </div>
+      </div>
+    </>
   );
 }
