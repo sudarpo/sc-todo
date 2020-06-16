1ffba1c72e9d22a52e028d04c71e567df2a7b1b8
sudarpo chong <shc221b@gmail.com>
2020-06-16 18:23:59 +0800

    Added gh-pages. Fixes on NavBar.

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -18,7 +18,7 @@ export default function NavBar() {
 
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-      <a className="navbar-brand" href="/#">
+      <a className="navbar-brand" href="#home">
         FYM | Forgot Your Milk
       </a>
       <button
@@ -36,17 +36,17 @@ export default function NavBar() {
       <div className={isCollapsed ? "collapse navbar-collapse" : "collapse navbar-collapse show"} id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item active">
-            <a className="nav-link" href="/#" onClick={() => handleChangeMode("AllTasks")}>
+            <a className="nav-link" href="#allTasks" onClick={() => handleChangeMode("AllTasks")}>
               All Tasks <span className="sr-only">(current)</span>
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/#" onClick={() => handleChangeMode("NewTask")}>
+            <a className="nav-link" href="#newTask" onClick={() => handleChangeMode("NewTask")}>
               New Task
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/#" onClick={() => handleChangeMode("Login")}>
+            <a className="nav-link" href="#login" onClick={() => handleChangeMode("Login")}>
               Login
             </a>
           </li>
